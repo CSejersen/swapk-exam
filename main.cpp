62b5c4773e23547f53c3980cbc46f35c274fa867
Christian <cvsejersen@gmail.com>
2025-11-28 10:37:48 +0100

    design material class

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <iostream>
+#include "RawMaterial.hpp"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+    auto material = Factory::Data::RawMaterial("Titanium_slab", 100.0_kg, 1024);
+    auto material2 = Factory::Data::RawMaterial(std::move(material));
+    auto material3 = Factory::Data::RawMaterial("otherstuff", 100.0_kg, 1024);
+    material3 = std::move(material2);
 }
